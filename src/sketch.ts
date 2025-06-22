3599b64e27e8771d2900a39691d5d1163632580c
Antony74 <akb@akb.me.uk>
2025-06-22 20:35:39 +0100

    pentagon

--- src/sketch.ts
+++ src/sketch.ts
@@ -46,43 +46,17 @@ new p5((p) => {
         p.colorMode(p.HSB);
     };
 
-    const animationLength = 1000;
-    const animationPhases = 3;
-    const animationPhaseLength = animationLength / animationPhases;
-
     p.draw = () => {
         p.translate(0.5 * p.width, 0.5 * p.height);
         p.background(0, 0, 95);
         p.noFill();
         p.strokeWeight(10);
 
-        const phase = Math.floor(
-            (p.frameCount % animationLength) / animationPhaseLength,
-        );
+        const size = 100;
 
-        const absolutePhase = Math.floor(p.frameCount / animationPhaseLength);
+        const u = space.createVector(size, 0);
 
-        const frame = p.frameCount % animationPhaseLength;
-        const alpha = p.map(frame, 0, animationPhaseLength, 4, 0);
-
-        switch (phase) {
-            case 0:
-                p.stroke(0, 255, 255, alpha);
-                break;
-            case 1:
-                p.stroke(30, 255, 255, alpha);
-                break;
-            case 2:
-                p.stroke(220, 255, 255, alpha);
-                break;
-        }
-
-        const size = p.map(frame, 0, animationPhaseLength, 0, 300);
-
-        const sign = absolutePhase % 2 ? 1 : -1;
-
-        const u = space.createVector(size, 0).rotate((sign * frame) / 100);
-
-        drawPolygon(u, phase + 3);
+        drawPolygon(u, 5);
+        p.noLoop();
     };
 });
